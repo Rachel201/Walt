25b9e69c00b0101cccb7ceb50510cdd143d5d93b
Rachel201 <87396705+Rachel201@users.noreply.github.com>
2021-08-02 11:13:11 +0300

    Add files via upload

---
+++ src/main/java/com/walt/WaltServiceImpl.java
@@ -0,0 +1,123 @@
+package com.walt;
+
+import com.walt.dao.DeliveryRepository;
+import com.walt.dao.DriverRepository;
+import com.walt.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.util.*;
+
+@Service
+public class WaltServiceImpl implements WaltService {
+
+    @Autowired
+    private DeliveryRepository deliveryRepository;
+    @Autowired
+    private DriverRepository driverRepository;
+
+    /**
+     *The function create order and assign driver
+     * The city must to be in restaurant and find available driver for dlivery
+     * @param customer
+     * @param restaurant
+     * @param deliveryTime
+     * @return
+     */
+    @Override
+    public Delivery createOrderAndAssignDriver(Customer customer, Restaurant restaurant, Date deliveryTime) {
+        if (customer == null || restaurant == null || deliveryTime == null) {
+            throw new RuntimeException("Error: One or more of the details  is null");
+        }
+        if (!customer.getCity().getId().equals(restaurant.getCity().getId())) {
+            throw new RuntimeException("Error:The restaurant should be in your city of located");
+        }
+        List <Driver>driversAvailable=findDriversAvailable(customer,deliveryTime);
+        Driver driver =getDriverMostBusy(driversAvailable);
+        Delivery delivery = new Delivery(driver, restaurant, customer, deliveryTime);
+        deliveryRepository.save(delivery);
+        return delivery;
+    }
+
+    /**
+     *The function search driver that live in same city of customer and restaurant
+     * @param customer
+     * @param deliveryTime
+     * @return
+     */
+    public List<Driver>findDriversAvailable(Customer customer, Date deliveryTime)
+    {
+        List<Driver>driversAvailable = new ArrayList<Driver>();
+        for (Driver driver :driverRepository.findAllDriversByCity(customer.getCity())){
+             if(isDriverAvailable(driver,deliveryTime)){
+                 driversAvailable.add(driver);
+             }
+        }
+        if (driversAvailable.isEmpty()) {
+            throw new RuntimeException("Error: There is no driver available in the city");
+        }
+        return  driversAvailable;
+    }
+
+    /**
+     *The function check if driver available to delivery according time
+     * @param driver
+     * @param deliveryTime
+     * @return
+     */
+    public boolean isDriverAvailable(Driver driver,Date deliveryTime){
+            List<Delivery> deliveries = deliveryRepository.findDeliveriesByDriver(driver);
+            for (Delivery delivery : deliveries) {
+                long MillionSeconds = Math.abs(delivery.getDeliveryTime().getTime() - deliveryTime.getTime());
+
+                //1000ms * 60s * 2m
+                if (MillionSeconds <= (1000 * 60 * 60)) {
+                    return false;
+                }
+            }
+            return true;
+    }
+
+    /**
+     *
+     * @param availableDrivers
+     * @return
+     */
+    private Driver getDriverMostBusy(List<Driver> availableDrivers) {
+        Collections.sort(availableDrivers, Comparator.comparingDouble(this::getDriverKM));
+        return availableDrivers.get(0);
+    }
+
+    /**
+     *
+     * @param driver
+     * @return
+     */
+    private double getDriverKM(Driver driver) {
+        return deliveryRepository.findDeliveriesByDriver(driver).stream()
+                .mapToDouble(Delivery::getDistance)
+                .sum();
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public List<DriverDistance> getDriverRankReport() {
+        return deliveryRepository.findDistancesByDriver();
+    }
+
+    /**
+     *
+     * @param city
+     * @return
+     */
+    @Override
+    public List<DriverDistance> getDriverRankReportByCity(City city) {
+         return deliveryRepository.findCityDistancesByDriver(city);
+    }
+
+
+}
---
+++ src/test/java/com/walt/WaltTest.java
@@ -0,0 +1,262 @@
+package com.walt;
+
+import com.walt.dao.*;
+import com.walt.model.*;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.annotation.DirtiesContext;
+
+import javax.annotation.Resource;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+@SpringBootTest()
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class WaltTest {
+
+    @TestConfiguration
+    static class WaltServiceImplTestContextConfiguration {
+
+        @Bean
+        public WaltService waltService() {
+            return new WaltServiceImpl();
+        }
+    }
+
+    @Autowired
+    WaltService waltService;
+
+    @Resource
+    CityRepository cityRepository;
+
+    @Resource
+    CustomerRepository customerRepository;
+
+    @Resource
+    DriverRepository driverRepository;
+
+    @Resource
+    DeliveryRepository deliveryRepository;
+
+    @Resource
+    RestaurantRepository restaurantRepository;
+
+    @BeforeEach()
+    public void prepareData(){
+
+        City jerusalem = new City("Jerusalem");
+        City tlv = new City("Tel-Aviv");
+        City bash = new City("Beer-Sheva");
+        City haifa = new City("Haifa");
+
+        cityRepository.save(jerusalem);
+        cityRepository.save(tlv);
+        cityRepository.save(bash);
+        cityRepository.save(haifa);
+
+
+        createDrivers(jerusalem, tlv, bash, haifa);
+
+        createCustomers(jerusalem, tlv, haifa,bash);
+
+        createRestaurant(jerusalem, tlv ,bash);
+    }
+
+    private void createRestaurant(City jerusalem, City tlv, City bash) {
+        Restaurant meat = new Restaurant("meat", jerusalem, "All meat restaurant");
+        Restaurant vegan = new Restaurant("vegan", tlv, "Only vegan");
+        Restaurant cafe = new Restaurant("cafe", tlv, "Coffee shop");
+        Restaurant chinese = new Restaurant("chinese", tlv, "chinese restaurant");
+        Restaurant mexican = new Restaurant("restaurant", tlv, "mexican restaurant ");
+        Restaurant cook = new Restaurant("cook", bash, "cookies chockalate");
+
+        restaurantRepository.saveAll(Lists.newArrayList(meat, vegan, cafe, chinese, mexican,cook));
+    }
+
+    private void createCustomers(City jerusalem, City tlv, City haifa,City bash) {
+        Customer beethoven = new Customer("Beethoven", tlv, "Ludwig van Beethoven");
+        Customer mozart = new Customer("Mozart", jerusalem, "Wolfgang Amadeus Mozart");
+        Customer chopin = new Customer("Chopin", haifa, "Frédéric François Chopin");
+        Customer rachmaninoff = new Customer("Rachmaninoff", tlv, "Sergei Rachmaninoff");
+        Customer bach = new Customer("Bach", tlv, "Sebastian Bach. Johann");
+        Customer gilli = new Customer("Gilli", bash, "gilli shalom");
+        Customer shared = new Customer("Shared", bash, "golomb ferg");
+
+        customerRepository.saveAll(Lists.newArrayList(beethoven, mozart, chopin, rachmaninoff, bach,gilli,shared));
+    }
+
+    private void createDrivers(City jerusalem, City tlv, City bash, City haifa) {
+        Driver mary = new Driver("Mary", tlv);
+        Driver patricia = new Driver("Patricia", tlv);
+        Driver jennifer = new Driver("Jennifer", haifa);
+        Driver james = new Driver("James", bash);
+        Driver john = new Driver("John", bash);
+        Driver robert = new Driver("Robert", jerusalem);
+        Driver david = new Driver("David", jerusalem);
+        Driver daniel = new Driver("Daniel", tlv);
+        Driver noa = new Driver("Noa", haifa);
+        Driver ofri = new Driver("Ofri", haifa);
+        Driver nata = new Driver("Neta", jerusalem);
+        Driver json = new Driver("json", tlv);
+
+        driverRepository.saveAll(Lists.newArrayList(mary, patricia, jennifer, james, john, robert, david, daniel, noa, ofri, nata,json));
+    }
+
+    @Test
+    public void testBasics(){
+
+        assertEquals(((List<City>) cityRepository.findAll()).size(),4);
+        assertEquals((driverRepository.findAllDriversByCity(cityRepository.findByName("Beer-Sheva")).size()), 2);
+    }
+    @Test
+    public void createOrder() {
+        Customer customer = customerRepository.findByName("Bach");
+        Restaurant restaurant = restaurantRepository.findByName("cafe");
+        Date date = new Date();
+        Driver testDriver = driverRepository.findByName("Mary");
+        Delivery delivery = waltService.createOrderAndAssignDriver(customer, restaurant, date);
+        assertEquals(testDriver.getId(), delivery.getDriver().getId());
+    }
+
+    @Test
+    public void createTowOrderNotAtTheSameTime(){
+        Customer customer1 = customerRepository.findByName("Beethoven");
+        Customer customer2 = customerRepository.findByName("Bach");
+        Restaurant restaurant = restaurantRepository.findByName("meat");
+        Date date = new Date();
+        Driver driver = driverRepository.findByName("Robert");
+
+        Delivery delivery = waltService.createOrderAndAssignDriver(customer1, restaurant,date);
+        Date date2 = new Date();
+        date2.setTime(date2.getTime() + TimeUnit.HOURS.toMillis(3));
+
+        Delivery delivery2 = waltService.createOrderAndAssignDriver(customer2, restaurant, date2);
+
+        assertEquals(driver.getId(), delivery.getDriver().getId());
+    }
+    @Test
+    public void createTowOrders(){
+        Customer customer1 = customerRepository.findByName("Beethoven");
+        Customer customer2 = customerRepository.findByName("Bach");
+        Restaurant restaurant = restaurantRepository.findByName("cafe");
+        Date date = new Date();
+        Driver driver = driverRepository.findByName("Robert");
+
+        Delivery delivery = waltService.createOrderAndAssignDriver(customer1, restaurant,date);
+
+        Delivery delivery2 = waltService.createOrderAndAssignDriver(customer2, restaurant, date);
+
+        assertNotEquals(driver.getId(), delivery.getDriver().getId());
+    }
+
+
+    @Test
+    public void createOrderRestaurantNotInTown() {
+        Customer customer = customerRepository.findByName("Beethoven");
+        Date date = new Date();
+        Restaurant restaurant = restaurantRepository.findByName("meat");
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            waltService.createOrderAndAssignDriver(customer, restaurant, date);
+        });
+
+        String expectedMessage = "Error: The restaurant should be in your city of located";
+        String actualMessage = exception.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public  void avaibleDriver() {
+        Customer customer = customerRepository.findByName("Gilli");
+
+        Restaurant restaurant = restaurantRepository.findByName("cook");
+        Date date = new Date();
+
+        Delivery delivery1 = waltService.createOrderAndAssignDriver(customer, restaurant, date);
+
+        Delivery delivery2 = waltService.createOrderAndAssignDriver(customer, restaurant, date);
+
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            waltService.createOrderAndAssignDriver(customer, restaurant, date);
+        });
+
+        String expectedMessage = "Error: There is no driver available in the city";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+
+
+    @Test
+    public void testDriverRankReportByCity() {
+
+        Customer customer1 = customerRepository.findByName("Gilli");
+        Customer customer2 = customerRepository.findByName("Shared");
+
+        Restaurant restaurant = restaurantRepository.findByName("cook");
+        Date date = new Date();
+        date.setTime(date.getTime() + TimeUnit.HOURS.toMillis(10000));
+
+
+        Delivery delivery1 = waltService.createOrderAndAssignDriver(customer1, restaurant,  date);
+
+        Delivery delivery2 = waltService.createOrderAndAssignDriver(customer2, restaurant,  date);
+
+
+        City city = cityRepository.findByName("Beer-Sheva");
+
+        List<DriverDistance> driverDistancesByCity = waltService.getDriverRankReportByCity(city);
+
+
+        System.out.println("\nTotal distance of delivery order by total distance in descending order by city" +
+                city.getName() +
+                ":");
+        System.out.println(
+                "----------------------------------------------------------------------\n");
+
+        for (DriverDistance d : driverDistancesByCity) {
+            System.out.println("Name: " + d.getDriver().getName() +
+                    ", Distance: " + d.getTotalDistance());
+        }
+    }
+    @Test
+    public void driverRankReport() {
+        Customer customer1 = customerRepository.findByName("Gilli");
+        Customer customer2 = customerRepository.findByName("Mozart");
+
+        Restaurant restaurant1 = restaurantRepository.findByName("cook");
+        Restaurant restaurant2 = restaurantRepository.findByName("meat");
+
+        Date date = new Date();
+        date.setTime(date.getTime() + TimeUnit.HOURS.toMillis(2000));
+
+
+        Delivery delivery1 = waltService.createOrderAndAssignDriver(customer1, restaurant1, date);
+
+        Delivery delivery2 = waltService.createOrderAndAssignDriver(customer2, restaurant2,date);
+
+
+        List<DriverDistance> driverDistances = waltService.getDriverRankReport();
+
+        System.out.println("Drivers rank rebort By distance:");
+        System.out.println("--------------------------------------------------");
+
+        for (DriverDistance d : driverDistances) {
+            System.out.println(" Name: " + d.getDriver().getName() +
+                    ", Distance: " + d.getTotalDistance());
+        }
+    }
+
+
+}
+
+
